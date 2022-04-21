a8c782d9a8ddac01f0291d7f4f528e7bc0e5bfd9
0xdecafdad <shaunderstine@gmail.com>
2022-04-21 03:06:06 -0400

    Add function get_edpi to build url in scraper.py and demo functionality in scraper_drive.py

--- scraper.py
+++ scraper.py
@@ -23,9 +23,32 @@ def convert_url(url):
 # Recursive function to calculate the average of a list of ints
 def calc_average(ints, i, size):
 	if i == size-1:
-		return ints[x]
-	else if i == 0:
-		return ( ls[x] + calc_average(ls, x+1, size) ) / size
+		return ints[i]
+	elif i == 0:
+		return ( ints[i] + calc_average(ints, i+1, size) ) / size
 	else:
-		return ls[x] + calc_average(ls, x+1, size)
+		return ints[i] + calc_average(ints, i+1, size)
 # end calc_average
+
+# Finds average edpi of all listed pro players for {game}
+# Game is a string, either "valorant", "apex", or "csgo"
+def get_edpi(game):
+	# Gather components to craft url of webpage with approriate edpi list
+	pre = 'https://prosettings.net/'
+	game_name = ''
+	# Will have to have input validation in driver code
+	if game == 'valorant':
+		game_name = 'valorant'
+	elif game == 'apex':
+		game_name = 'apex-legends'
+	elif game == 'csgo':
+		game_name = 'cs-go'
+	else:
+		# Later, raise (maybe custom) exception here
+		# InvalidGame("Game must be 'valorant', 'apex', or 'csgo'")
+		print('Error: Invalid Game!')
+	post = '-pro-settings-gear-list/'
+	url = '{pre}{game_name}{post}'.format(pre=pre, game_name=game_name, post=post)
+
+	print(url)
+# end get_edpi()
--- scraper_driver.py
+++ scraper_driver.py
@@ -2,8 +2,31 @@
 # Date Created:	4/21/2022
 # Description:	Driver for scraper.py
 
-def main():
-	
+from scraper import *
 
-if __name__ = "__main__":
+def main():
+	# Input validation loop
+	# Users must select either VALORANT, Apex Legends, or CS:GO from list
+	while True:
+		print('Select game:')
+		print('(1) VALORANT')
+		print('(2) Apex Legends')
+		print('(3) Counter Strike: Global Offensive')
+		game = input('\n> ')
+		
+		# If valid selection, get edpis for appropriate game and break loop
+		# Otherwise, repeat selection
+		if game == '1':
+			get_edpi('valorant')
+			break
+		elif game == '2':
+			get_edpi('apex')
+			break
+		elif game == '3':
+			get_edpi('csgo')
+			break
+		else:
+			print('Invalid selection. Please select a number 1-3.\n')
+
+if __name__ == "__main__":
 	main()
